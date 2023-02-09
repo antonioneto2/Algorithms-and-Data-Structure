a9404a379b377c0ff7d88acf01a673eaa6970126
Antonio Neto <113268997+antonioneto2@users.noreply.github.com>
2023-02-09 15:59:19 -0300

    new lines

--- struct/funcionarios.c
+++ struct/funcionarios.c
@@ -20,7 +20,7 @@ void preenche(Funcionario *dados, int qtd)
         scanf(" %f", &dados[index].salario);
         printf("Digite o cargo do funcionario %d: \n", index + 1);
         scanf(" %[^\n]s", dados[index].cargo);
-        printf("Digite o identificador do funcionario %d: \n", index + 1);
+        printf("Digite o Id do funcionario %d: \n", index + 1);
         scanf("%d", &dados[index].id);
     }
 }
@@ -31,8 +31,45 @@ void imprime(Funcionario *imprime, int qtd)
     for (index = 0; index < qtd; index++)
     {
         printf("\n");
-        printf("Funcionario %d || Nome: %s || Salario: %.2f || Cargo: %s || Identificador: %d", index + 1, imprime[index].nome, imprime[index].salario, imprime[index].cargo, imprime[index].id);
+        printf("Funcionario %d | Nome: %s | Salario: %.2f | Cargo: %s | Id: %d", index + 1, imprime[index].nome, imprime[index].salario, imprime[index].cargo, imprime[index].id);
     }
+}
+
+void muda_salario(Funcionario *sal, int quant)
+{
+    int i, alteracao;
+    printf("\n");
+    printf("deseja alterar o valor do salario dos funcionarios?(use 1 para sim e 2 para nao)\n");
+    scanf("%d", &alteracao);
+    if (alteracao == 2)
+    {
+        printf("o salario nao foi alterado");
+    }
+    else
+    {
+        for (i = 0; i < quant; i++)
+        {
+            printf("digite o novo salario do %d funcionario: ", i + 1);
+            scanf("%f", &sal[i].salario);
+        }
+    }
+}
+
+void maior_salario(Funcionario *maiorsal, int quant)
+{
+    int i;
+    float maiors = 0;
+    char mcargo[50];
+    for (i = 0; i < quant; i++)
+    {
+        if (maiorsal[i].salario > maiors)
+        {
+            maiors = maiorsal[i].salario;
+            strcpy(mcargo, maiorsal[i].cargo);
+        }
+    }
+    printf("\n");
+    printf("o cargo da pessoa com o maior salario e: %s e o salario dessa pessoa e: %f\n", mcargo, maiors);
 }
 
 int main()
@@ -43,4 +80,6 @@ int main()
     Funcionario *dados_funcionario = (Funcionario *)malloc(qtd_funcionario * sizeof(Funcionario));
     preenche(dados_funcionario, qtd_funcionario);
     imprime(dados_funcionario, qtd_funcionario);
+    muda_salario(dados_funcionario, qtd_funcionario);
+    maior_salario(dados_funcionario, qtd_funcionario);
 }
